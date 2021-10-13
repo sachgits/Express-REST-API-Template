e08912f0d9a6710788e01e5e442cfb4ff776f4ec
Alex Gitonga <sachgits@gmail.com>
2021-10-13 16:04:58 +0000

    worked on confirmation route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const axios = require("axios");
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send({ message: 'Hello world' });
 });
+
+router.post('/confirmation', (req, res)=> {
+    console.log("we hit confirmation .............http://killbill:8080/plugins/hello-world-plugin/Confirmation");
+    axios({
+        url: 'http://killbill:8080/plugins/hello-world-plugin/Confirmation',
+        method: 'post',
+        data: req.body,
+        headers: { 'X-Killbill-ApiKey': 'bob', 'X-Killbill-ApiSecret': 'lazar', 'X-Killbill-CreatedBy': 'Admin' },
+        auth: { username: 'admin', password: 'password' }
+      })
+        .then(function(response) {
+          console.log('SUCCESS went and cameback with no error');
+          res.status(200).json(response);
+        })
+        .catch(function(error) {
+          console.error('something went wrong with axios http request');
+          res.status(500).json(error);
+        });
+})
+
+
 
 module.exports = router;
